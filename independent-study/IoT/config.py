ff41f4a07237b66e1de548c499cfab19c320eced
abhiabhi15 <abhishek.agrawal15@yahoo.com>
2016-02-21 20:48:46 -0500

    Adding revised files

---
+++ independent-study/IoT/config.py
@@ -0,0 +1,6 @@
+
+FILEPATH = "/home/abhishek/PycharmProjects/programs/data/"
+OUTPUT_FILE = "winter_beijing_predicted_aqi.csv"
+MIN_DIST = 0.2
+START_DAY = 274
+END_DAY = 281
--- independent-study/IoT/mygraph.py
+++ independent-study/IoT/mygraph.py
@@ -1,5 +1,3 @@
-__author__ = 'abhishek'
-
 import networkx as nx
 import pandas as pd
 import math
@@ -8,49 +6,72 @@ import numpy as np
 from scipy.spatial import distance
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import mean_squared_error
-import aqi_index as aq
-import matplotlib.pyplot as plt
+import config as cfg
 
-#feature_list = ["temperature", "humidity", "wind_speed"]
 feature_list = ["weather", "temperature", "pressure", "humidity", "wind_speed", "wind_direction"]
 aqi_list = ["PM25_Concentration", "PM10_Concentration", "NO2_Concentration", "CO_Concentration", "O3_Concentration",
                "SO2_Concentration"]
-info_list = ["id", "label", "time", "station_id", "latitude", "longitude", "aqi"]
-
+info_list = ["id", "label", "time", "station_id", "latitude", "longitude"]
 v_list, u_list = [], []  # Storing Nodes (Labeled = v_list, Unlabeled = u_list)
 edge_feature_DF, feature_params = None, None
-MIN_DIST = 0.35
 LABEL_KNOWN = "known"
 LABEL_UNKNOWN = "unknown"
 AQI_POLLUTANTS = "aqi_pollutants"
 FEATURES = "features"
 
-def get_labeled_node_edges():
+def is_same_layer(node1, node2):
+    return node1.split("_")[1] == node2.split("_")[1]
+
+def is_same_station(node1, node2):
+    return node1.split("_")[0] == node2.split("_")[0]
+
+def connect_labeled_node_edges(G):
     edges = []
     for vnode in v_list:
         for unode in u_list:
-            t = vnode, unode, 1
-            edges.append(t)
-    return edges
+            if is_same_layer(unode, vnode):
+                t = vnode, unode, 1
+                edges.append(t)
+    if len(edges) > 0:
+        G.add_weighted_edges_from(edges)
 
 def compute_distance(t1, t2):
     return math.sqrt(math.pow(t1[0] - t2[0], 2) + math.pow(t1[1] - t2[1], 2))
 
-def get_neighbor_node_edges(G):
+def connect_neighbor_node_edges(G):
     edges = []
     for k1, v1 in G.nodes_iter(data=True):
         for k2, v2 in G.nodes_iter(data=True):
-            if k1 != k2:
+            if k1 != k2 and is_same_layer(k1, k2):
                 dist = compute_distance([v1["latitude"], v1["longitude"]],
                                        [v2["latitude"], v2["longitude"]])
-                if dist <= MIN_DIST and not(G.has_edge(k1, k2)):
+                if dist <= cfg.MIN_DIST and not(G.has_edge(k1, k2)):
                     t = k1, k2, 1
                     edges.append(t)
-    return edges
+    if len(edges) > 0:
+        G.add_weighted_edges_from(edges)
+
+def connect_time_layer_node_edges(G):
+    edges = []
+    for unode in u_list:
+        for other_unode in u_list:
+            if is_same_station(unode, other_unode):
+                 t = unode, other_unode, 1
+                 edges.append(t)
+
+    for vnode in u_list:
+        for other_vnode in u_list:
+            if is_same_station(vnode, other_vnode):
+                 t = vnode, other_vnode, 1
+                 edges.append(t)
+    if len(edges) > 0:
+        G.add_weighted_edges_from(edges)
+
 
 def add_graph_edges(G):
-    G.add_weighted_edges_from(get_labeled_node_edges()) # Adding edges from Labeled to unlabeled nodes
-    G.add_weighted_edges_from(get_neighbor_node_edges(G)) #Adding Neighbourhood Edges
+    connect_labeled_node_edges(G) # Adding edges from Labeled to unlabeled nodes
+    connect_neighbor_node_edges(G) # Adding Neighbourhood Edges
+    connect_time_layer_node_edges(G) # Adding Different Time Layer Edges
 
 def extract_properties(r):
     dict = r.to_dict()
@@ -67,9 +88,8 @@ def extract_properties(r):
     props[FEATURES] = fdict
     return props
 
-def create_graph(df):
+def add_graph_nodes(G, df):
 
-    G = nx.Graph()
     row_iterator = df.iterrows()
     for i, row in row_iterator:
         props = extract_properties(row)
@@ -78,7 +98,6 @@ def create_graph(df):
         elif props['label'] == LABEL_UNKNOWN:
             u_list.append(props['id'])
         G.add_node(row['id'], props)
-    add_graph_edges(G)
     return G
 
 def create_edge_feature_DF(G):
@@ -103,8 +122,6 @@ def learn_feature_params(feature_DF):
     feature_params = {}
     for feature in feature_list:
         fDF = feature_DF.loc[(feature_DF['fname'] == feature) & (feature_DF['label'] == LABEL_KNOWN)].dropna()
-        # plt.scatter(fDF["feature_diff"], fDF["aqi_sim"])
-        # plt.show()
         if fDF.empty:
             result.params = [0, 0]
         else:
@@ -133,29 +150,21 @@ def init_edge_weights(G):
 
 
 def preprocess_DF(df):
-    df.replace('NULL', np.nan)
-    df = df.fillna(df.mean())
-    df[feature_list] = df[feature_list].apply(lambda x: MinMaxScaler().fit_transform(x))
-    for pollutant in aq.pollutants:
-        df[pollutant] =  df[pollutant].apply(lambda x: aq.get_aqi_value(x, pollutant))
-
-    df[["aqi"]] = df[["aqi"]].apply(lambda x: MinMaxScaler().fit_transform(x))
-    return df
+   df.replace('NULL', np.nan)
+   df = df.fillna(df.mean())
+   df[feature_list] = df[feature_list].apply(lambda x: MinMaxScaler().fit_transform(x))
+   return df
 
 def calc_aqi_distribution(G):
-
     pu_list = []
     for unode in u_list:
         p_arr = None
         for vnode in v_list:
-
             if G.has_edge(unode, vnode):
                 pv_list = []
-
                 wt = G[unode][vnode]['weight']
                 for k,v in G.node[vnode][AQI_POLLUTANTS].iteritems():
                     pv_list.append(wt * v)
-
                 p_temp = np.array(pv_list).reshape(1, len(aqi_list))
 
                 if p_arr is None:
@@ -174,26 +183,36 @@ def compute_rmse(predicted_list, G):
     actual_vals = []
     for unode in u_list:
         actual_vals.append(G.node[unode][AQI_POLLUTANTS].values())
-
     actual_list = [val for sublist in actual_vals for val in sublist]
-    print "Actual List = ", actual_list
-    print "Predicted List = ", predicted_list
-    rms = math.sqrt(mean_squared_error(actual_list, predicted_list))
-    print "RMSE = " , rms
+    return math.sqrt(mean_squared_error(actual_list, predicted_list))
 
 
-def aqi_inference(df):
-    aq.init_aqi_index()
-    df = preprocess_DF(df)
-    G = create_graph(df)
+def write_predicted_results(predicted_list):
+    arr = np.array(predicted_list).reshape(len(u_list), len(aqi_list))
+    predicted_df = pd.DataFrame(arr, columns=aqi_list)
+    predicted_df.insert(0, "station_id", u_list)
+    predicted_df.to_csv(cfg.OUTPUT_FILE, index=False)
+
+
+def aqi_inference():
+    G = nx.Graph() # Instantiating Graph Object
+    for day in range(cfg.START_DAY, cfg.END_DAY, 1):
+        file_name = cfg.FILEPATH + "winter_beijing_T" + `day` + ".csv"
+        df = pd.read_csv(file_name, sep=',')
+        df = preprocess_DF(df)
+        G = add_graph_nodes(G, df)
+    add_graph_edges(G)
     init_edge_weights(G)
     predicted_list = calc_aqi_distribution(G)
-    compute_rmse(predicted_list, G)
+    rmse = compute_rmse(predicted_list, G)
+    write_predicted_results(predicted_list)
+
+    print "MIN_DIST = ", cfg.MIN_DIST
+    print "RMSE = ", rmse
     print "Number of edges = ",  G.number_of_edges()
-    print "Number of nodes = ",  G.number_of_nodes()
+    print "Total Number of nodes = ",  G.number_of_nodes()
+    print "Total Number of unknown nodes = ",  len(u_list)
     G.clear()
 
-
 if __name__ == '__main__':
-    df = pd.read_csv('/home/abhishek/PycharmProjects/programs/data/winter_beijing_T274.csv', sep=',')
-    aqi_inference(df)
+    aqi_inference()
